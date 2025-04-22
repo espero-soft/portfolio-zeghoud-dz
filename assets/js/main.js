9a423211f22fb2d61ea09ae26aa23fd997ee6e9c
Zeghoud-dz <tidafamdah@gmail.com>
2025-04-22 20:22:39 +0200

    initial-mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-// mon script
+
+const iconToggleMobile = document.getElementById("toggleMobile")
+const closeMobileNavs = [].slice.call(document.getElementsByClassName("mobile-close"))
+
+
+const toggleNav = ()=>{
+    const mobileNav = document.getElementsByClassName("mobile")[0]
+    if(!mobileNav.classList.contains("none")){
+        mobileNav.classList.toggle("close")
+        setTimeout(()=>{
+            mobileNav.classList.toggle("none")
+            mobileNav.classList.toggle("close")
+            document.body.style.overflow = ""
+        },2000)
+    }else{
+        mobileNav.classList.toggle("none")
+        document.body.style.overflow = "hidden"
+    }
+}
+
+iconToggleMobile.addEventListener("click", toggleNav)
+
+closeMobileNavs.forEach(item => {
+    item.addEventListener("click", toggleNav)
+});
+
